f55b65c396c4f78e296ff12dba65e352a11b1b21
Jake <jake098877@gmail.com>
2021-09-13 18:23:17 -0400

    Fetch is POST called without redux.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -7,18 +7,15 @@ export const setCurrentUser = (user) => {
 
 export const login = (credentials) => {
   console.log(credentials);
-  return (dispatch) => {
-    return fetch("http://localhost:3000 /api/v1/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    })
-      .then((r) => r.json())
-      .then((response) =>
-        console.log("DKJLNGDSA", dispatch(setCurrentUser(response)))
-      )
-      .catch((err) => err);
-  };
+
+  return fetch("http://localhost:3000 /api/v1/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then((r) => r.json())
+    .then((response) => console.log("DKJLNGDSA"))
+    .catch((err) => err);
 };
