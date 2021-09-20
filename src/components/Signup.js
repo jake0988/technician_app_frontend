a5e706d0ae3b936c0da634bbc98ac9cddb854b77
Jake <jake098877@gmail.com>
2021-09-20 16:23:33 -0400

    Adds Signup component.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,55 @@
 import React from "react";
+import { connect } from "react-redux";
+import { updateSignupForm } from "../actions/signupForm";
+import { signup } from "../actions/currentUser";
 
-const Signup = () => {
+const Signup = ({ signupFormData, updateSignupForm, signup }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    signup(signupFormData);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const updateFormInfo = {
+      ...signupFormData,
+      [name]: value,
+    };
+    updateSignupForm(updateFormInfo);
+  };
+
   return (
-    <form>
-      <input type="submit">Signout</input>
+    <form onSubmit={handleClick}>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={signupFormData.name}
+        name="name"
+        placeholder="Name:"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        value={signupFormData.username}
+        name="username"
+        placeholder="Username:"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        value={signupFormData.password}
+        placeholder="Password:"
+        name="password"
+      />
+      <input type="submit" value="Sign Up" />
     </form>
   );
 };
 
-export default Signup;
+const mapStateToProps = (state) => {
+  return {
+    signupFormData: state.signupForm,
+  };
+};
+
+export default connect(mapStateToProps, { updateSignupForm, signup })(Signup);
