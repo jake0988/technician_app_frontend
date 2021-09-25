f3dd5c234edf534c071399cf7fd7dad0e28e8336
Jake <jake098877@gmail.com>
2021-09-25 17:36:39 -0400

    React routes render NavBar and Customers.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser";
-import Nav from "./components/Nav";
+import Nav from "./components/NavBar";
 import MainContainer from "./components/MainContainer";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Signup from "./components/Signup";
 import { Home } from "./components/Home";
 import CustomerList from "./components/CustomerList";
+import AddCustomerForm from "./components/AddCustomerForm";
+import NavBar from "./components/NavBar";
+import CustomerCard from "./components/CustomerCard";
 
 class App extends Component {
   componentDidMount() {
@@ -21,20 +24,27 @@ class App extends Component {
   render() {
     const { loggedIn } = this.props;
     return (
-      <Router>
-        <div className="App">
-          <Nav />
-          <MainContainer />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/users/:id"
+            render={() => (loggedIn ? <NavBar /> : <Home />)}
+          />
+
+          <Route exact path="/customers/new" component={AddCustomerForm} />
+          <Route exact path="/customers" component={CustomerList} />
+          <Route exact path="/customers/:id" component={CustomerCard} />
           <Route
             exact
             path="/"
-            render={() => (loggedIn ? <CustomerList /> : <Home />)}
+            render={() => (loggedIn ? <Nav /> : <Home />)}
           />
           <Route exact path="/login" component={Login} />
           <Route exact path="/logout" component={Logout} />
           <Route exact path="/signup" component={Signup} />
-        </div>
-      </Router>
+        </Switch>
+      </div>
     );
   }
 }
